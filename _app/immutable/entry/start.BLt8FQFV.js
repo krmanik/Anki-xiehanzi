e9ccadee6f4fce22437a2623760aaeb30ec5e161
krmanik <krmanik@users.noreply.github.com>
2026-08-25 09:15:23 +0000

    deploy: 4cd677f5328e06fe759ba38cac2511ae19e56885

--- _app/immutable/entry/start.BLt8FQFV.js
+++
@@ -1,1 +0,0 @@
-import{n as e,s as t}from"../chunks/B0p2h8kr.js";export{t as load_css,e as start};
---
+++ _app/immutable/entry/start.Cm80m5dI.js
@@ -0,0 +1,1 @@
+import{n as e,s as t}from"../chunks/CACqvMV2.js";export{t as load_css,e as start};
